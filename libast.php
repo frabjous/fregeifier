21b109a707fcdf172932f687b525d1b84ff752c8
Kevin Klement <klement@umass.edu>
2024-02-06 15:27:15 -0500

    kron push 2024-02-06 15:27:15

--- libast.php
+++ libast.php
@@ -1,5 +1,35 @@
 <?php
 
+function fregeify_ast($obj, $active) {
+    if (is_object($obj)) {
+        // see if do a change
+        if (($active) && isset($obj->t) && ($obj->t == 'Math')) {
+            // ensure a content component
+            if (!isset($obj->c)) {
+                return $obj;
+            }
+            $c = $obj->c;
+            //guard against weird math objects
+            if (!is_array($c)) {
+                return $obj;
+            }
+            if (count($c) < 2) {
+                return $obj;
+            }
+            if (!is_string($c[1])) {
+                return $obj;
+            $displaymath = true;
+            if (is_object($c[0]) && isset($c[0]->t) &&
+                (str_contains($c[0]->t, 'Inline'))) {
+                $displaymath = false;
+            }
+            $math_text = $c[1];
+            $img_file = get_image_file($math_text);
+        }
+    }
+    return $obj;
+}
+
 function get_extra_headers($obj, $eh) {
     if (is_array($obj)) {
         // check if header is a tex inclusion and grab it
@@ -29,3 +59,5 @@ function get_extra_headers($obj, $eh) {
     }
     return $eh;
 }
+
+
