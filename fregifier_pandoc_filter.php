f2dc9dfb246748dc86934cdb70c77703bf41000f
Kevin Klement <klement@umass.edu>
2024-02-06 15:07:16 -0500

    kron push 2024-02-06 15:07:16

--- fregifier_pandoc_filter.php
+++ fregifier_pandoc_filter.php
@@ -16,6 +16,9 @@ if (!$obj) {
 
 require_once('libast.php');
 
+file_put_contents('/home/kck/tmp/pre.json', json_encode($obj,
+    JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT));
+
 
 $extra_headers = '';
 if (isset($obj->meta->{'header-includes'})) {
@@ -23,5 +26,9 @@ if (isset($obj->meta->{'header-includes'})) {
 }
 
 file_put_contents('/home/kck/tmp/eh.txt', $extra_headers);
+
+file_put_contents('/home/kck/tmp/post.json', json_encode($obj,
+    JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT));
+
 
 echo json_encode($obj, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
